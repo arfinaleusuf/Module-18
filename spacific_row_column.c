f332119e552eb4aa20dbf880d4d284f5428410c4
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-10 00:29:42 +0600

    spacific row

---
+++ spacific_row_column.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d %d", &r,&c);
+    int a[r][c];
+    for(int i = 0; i<r; i++)
+    {
+        for(int j = 0; j<c; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int nRow = 0;
+    scanf("%d", &nRow);
+    for(int i = 0; i<c; i++)
+    {
+        printf("%d ", a[nRow][i]);
+    }
+
+    return 0;
+}
